73f0751bb100f0d9c5877f131f7ef4f08a5b8b31
aewhitfield90 <aewhitfield90@gmail.com>
2021-11-06 14:57:35 -0700

    added friends cuties

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ function clickup(){
     document.getElementById("cutie").src = "cutie.png";
 }
 function upgrade(){
-    if (click > check){
+    if (click >= check){
         click = click - check;
         count.innerHTML = click;
         up = up + 1;
         check = check*2;
         upgradeID.innerHTML = up+1;
         cost.innerHTML = check;
+        var cutiefriends = document.getElementById("friends");
+        cutiefriends.innerHTML +='<img src="cutie.png" alt="a cutie friend" id="cutie">';
     }
 }
