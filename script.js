cc6cd1f60f7538697f878c58e664d5146b07653c
aewhitfield90 <aewhitfield90@gmail.com>
2021-11-06 13:05:55 -0700

    upgrade button increases with each press

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var click = 0;
 var up = 0;
+var check = 10;
 function clicked(){
     click = click + up + 1;
     count.innerHTML = click;
 }
 function upgrade(){
-    if (click > 10){
-        click = click - 10;
+    if (click > check){
+        click = click - check;
         count.innerHTML = click;
         up = up + 1;
+        check = check*10;
         upgradeID.innerHTML = up+1;
+        cost.innerHTML = check;
     }
 }
